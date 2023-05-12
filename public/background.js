f51b19176be2e42072f8d125995ec9128d24c243
CrawlerCode <41094392+CrawlerCode@users.noreply.github.com>
2023-05-12 20:36:07 +0200

    feat: Open Feedback discussion on uninstall

--- public/background.js
+++ public/background.js
@@ -1,5 +1,6 @@
 chrome.runtime.onInstalled.addListener(({ reason }) => {
   if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
     chrome.runtime.openOptionsPage();
+    chrome.runtime.setUninstallURL("https://github.com/CrawlerCode/redmine-time-tracking/discussions/1");
   }
 });
