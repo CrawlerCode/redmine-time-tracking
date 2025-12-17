1a25a3f4dfb48c06b786694dcfdbbb993349675f
CrawlerCode <41094392+CrawlerCode@users.noreply.github.com>
2025-12-17 22:41:51 +0100

    ci: Set canary version

--- wxt.config.ts
+++ wxt.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
       tailwindcss(),
     ],
   }),
-  manifest: ({ browser }) => ({
+  manifest: ({ browser, mode }) => ({
     name: "__MSG_extName__",
     description: "__MSG_extDesc__",
     default_locale: "en",
@@ -36,9 +36,12 @@ export default defineConfig({
     homepage_url: "https://github.com/CrawlerCode/redmine-time-tracking",
     permissions: ["storage", "tabs", "activeTab", "scripting"],
     host_permissions: ["http://*/*", "https://*/*"],
-    /* ...(browser === "chrome" && {
-      key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk2PbuYr+evWm0U+t8XCn94jO0x0yydbdSqotCB9tC2NaYcnYhuOIrkDrAJg8DwUSvRbx2FXbacmf/CyI5fPivYmLDDL6D1VqabEhpVwNjTbj6V8lOy52C8Kl9ZA8WEvBZIsgU1bi54j2eg7B9lFZ3FIbavJB4Ezkny4eW+TdpUqcmoMxVybVeJ4q4jhzUFmEaHEmEyfzWLiUwYjEUzJkkzQYCTzZ0ZO1jWNnjfm5Y9IRSGPbzlChvigEhSuEs+DA2uZnr//mfr6xD97ryZY/i2HGeuxB3zZoIFR+FoFlp18ETJmEDO5ZEjEhl0Zb09yHlPah0IHDx6QjNcbxG3u31QIDAQAB",
-    }), */
+    ...(browser === "chrome" && {
+      key:
+        mode === "production"
+          ? "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk2PbuYr+evWm0U+t8XCn94jO0x0yydbdSqotCB9tC2NaYcnYhuOIrkDrAJg8DwUSvRbx2FXbacmf/CyI5fPivYmLDDL6D1VqabEhpVwNjTbj6V8lOy52C8Kl9ZA8WEvBZIsgU1bi54j2eg7B9lFZ3FIbavJB4Ezkny4eW+TdpUqcmoMxVybVeJ4q4jhzUFmEaHEmEyfzWLiUwYjEUzJkkzQYCTzZ0ZO1jWNnjfm5Y9IRSGPbzlChvigEhSuEs+DA2uZnr//mfr6xD97ryZY/i2HGeuxB3zZoIFR+FoFlp18ETJmEDO5ZEjEhl0Zb09yHlPah0IHDx6QjNcbxG3u31QIDAQAB"
+          : "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlm4d4x15I7iewgVp3cR51dCrwYINnvmmG/NjzK9Fg/LyzFLvRBqNftIqP4+fJas56CN2fNiFQNfdk8ACQJMKk8yd6fDeDs1/l/AseSWXM7GdbGDacj9lkZnL6jFHUrIxuwiHesE0W/I5o3Ep5ZXHnifjd7D9KEXswnvSWzmZXAtVNQu8DQXdNZO94N8i5ESLNdi78fmoeNIYaqieu5y0tlyMcvnOKiR3BHRAAN9bA0E/XvoazaL4KELcQbkh0T3nFhjSFDNHJAWlMkCvDGLC01RVLrkQW2ar9R5sAeyPmSx7HfgpzeBStwhDuOJ8s7Ed6WGCBaTll0WLQBxPYfV7qwIDAQAB",
+    }),
     ...(browser === "firefox" && {
       browser_specific_settings: {
         gecko: {
@@ -51,7 +54,11 @@ export default defineConfig({
   hooks: {
     "build:manifestGenerated": (wxt, manifest) => {
       if (wxt.config.mode === "development") {
-        manifest.name += " [DEV]";
+        manifest.name += " (DEV)";
+        manifest.version_name = `${manifest.version}-dev`;
+      } else if (wxt.config.mode === "canary") {
+        manifest.name += " (CANARY)";
+        manifest.version_name = `${manifest.version}-canary.${process.env.GITHUB_SHA?.slice(0, 7) ?? new Date().getTime()}`;
       }
     },
   },
