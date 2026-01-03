d66aa6ad7e41d1561941d505644db84d4396aa14
CrawlerCode <41094392+CrawlerCode@users.noreply.github.com>
2026-01-03 22:15:35 +0100

    feat(timers): Group timers by project and sort them by newest first

--- src/routes/timers.tsx
+++ src/routes/timers.tsx
@@ -1,10 +1,15 @@
+import { TReference } from "@/api/redmine/types";
+import Timer from "@/components/timer/Timer";
 import TimerSearch, { useTimerSearch } from "@/components/timer/TimerSearch";
 import { useIssuePriorities } from "@/hooks/useIssuePriorities";
 import { useSuspenseIssues } from "@/hooks/useIssues";
+import { groupTimers, TimersGroup } from "@/utils/groupTimers";
 import { createFileRoute } from "@tanstack/react-router";
+import clsx from "clsx";
+import { SquareChartGanttIcon } from "lucide-react";
+import { Fragment, useMemo } from "react";
 import { FormattedMessage } from "react-intl";
 import IssuesListSkeleton from "../components/issue/IssuesListSkeleton";
-import Timer from "../components/timer/Timer";
 import TimersBadge from "../components/timer/TimersBadge";
 import useTimers from "../hooks/useTimers";
 import { useSettings } from "../provider/SettingsProvider";
@@ -32,15 +37,22 @@ const TimersPage = () => {
   const search = useTimerSearch();
   const matchedTimers = timers.searchTimers(search, issues.data);
 
+  const groupedTimers = useMemo(() => groupTimers(matchedTimers, issues.data), [matchedTimers, issues.data]);
+
   return (
     <>
       <TimersBadge activeTimerCount={timers.getActiveTimerCount()} />
 
       <div className="flex flex-col gap-y-2">
-        {matchedTimers.map((timer) => {
-          const issue = issues.data.find((issue) => issue.id === timer.issueId);
-          return <Timer key={timer.id} timer={timer} issue={issue} issuePriorityType={issue ? issuePriorities.getPriorityType(issue) : undefined} variant="full" />;
-        })}
+        {groupedTimers.map(({ key, type, project, items }) => (
+          <Fragment key={key}>
+            <TimerProject type={type} project={project} />
+
+            {items.map(({ timer, issue }) => (
+              <Timer key={timer.id} timer={timer} issue={issue} issuePriorityType={issue ? issuePriorities.getPriorityType(issue) : undefined} variant="full" />
+            ))}
+          </Fragment>
+        ))}
 
         {matchedTimers.length === 0 && (
           <p className="text-center">
@@ -49,5 +61,27 @@ const TimersPage = () => {
         )}
       </div>
     </>
+  );
+};
+
+const TimerProject = ({ project, type }: { project?: TReference; type: TimersGroup["type"] }) => {
+  const { settings } = useSettings();
+
+  return (
+    <div
+      className={clsx("flex items-center gap-x-1", {
+        "bg-background shadow-background sticky top-0 z-5 -mx-2 -my-1 px-2 py-1 shadow": settings.style.stickyScroll,
+      })}
+    >
+      <SquareChartGanttIcon className="size-3.5 shrink-0" />
+
+      {project && (
+        <a href={`${settings.redmineURL}/projects/${project.id}`} target="_blank" tabIndex={-1} className="truncate text-sm hover:underline" rel="noreferrer">
+          {project.name}
+        </a>
+      )}
+
+      {type === "unknown-project" && <FormattedMessage id="timers.list.unknown-project-group" />}
+    </div>
   );
 };
---
+++ src/utils/groupTimers.ts
@@ -0,0 +1,72 @@
+import { TIssue, TReference } from "@/api/redmine/types";
+import { TimerController } from "@/hooks/useTimers";
+
+type TimerItem = {
+  timer: TimerController;
+  issue: TIssue | undefined;
+};
+
+export type TimersGroup = {
+  /**
+   * A unique key for the group
+   */
+  key: string;
+  /**
+   * The type of the group
+   */
+  type: "project" | "unknown-project";
+  /**
+   * The project associated with the group of timers
+   */
+  project?: TReference;
+  /**
+   * The list of timers and issues
+   */
+  items: TimerItem[];
+};
+
+/**
+ * Group timers by their associated issue's project
+ *
+ * @param timers - The list of timers to be grouped
+ * @param issues - The list of issues associated with the timers
+ * @returns An array of grouped timers by project
+ */
+export const groupTimers = (timers: TimerController[], issues: TIssue[]): TimersGroup[] => {
+  const reversedTimers = timers.toReversed(); // Show the most recent timers first
+
+  // Combine timers with their associated issues
+  const items = reversedTimers.map<TimerItem>((timer) => ({
+    timer,
+    issue: issues.find((issue) => issue.id === timer.issueId),
+  }));
+
+  // Group timers by project
+  const groups = Object.values(
+    items.reduce<
+      Record<
+        string,
+        TimersGroup & {
+          sortIdx: number;
+        }
+      >
+    >((groups, item) => {
+      const key = item.issue?.project.id ? `project-${item.issue.project.id}` : "unknown-project";
+
+      if (!groups[key]) {
+        groups[key] = {
+          key,
+          type: item.issue?.project.id ? "project" : "unknown-project",
+          project: item.issue?.project,
+          items: [],
+          sortIdx: Object.keys(groups).length,
+        };
+      }
+      groups[key].items.push(item);
+
+      return groups;
+    }, {})
+  ).sort((a, b) => a.sortIdx - b.sortIdx);
+
+  return groups;
+};
