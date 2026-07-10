c605b44dea6a67c39c30b71a1c95b55a64291dec
CrawlerCode <41094392+CrawlerCode@users.noreply.github.com>
2026-07-10 19:00:08 +0200

    feat(time): Add time entry chart and allow dynamic date rage (#73)

---
+++ src/components/time-entry/TimeEntryStatsCard.tsx
@@ -0,0 +1,43 @@
+import { TimeByProjectChart, TimeByProjectChartSkeleton } from "@/components/time-entry/TimeByProjectChart";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useIntl } from "react-intl";
+import { TTimeEntry } from "../../api/redmine/types";
+
+type PropTypes = {
+  entries: TTimeEntry[];
+};
+
+export const TimeEntryStatsCard = ({ entries }: PropTypes) => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <Card size="sm">
+      <CardHeader>
+        <CardTitle>{formatMessage({ id: "time.stats.title" })}</CardTitle>
+        <CardDescription className="max-sm:hidden">{formatMessage({ id: "time.stats.description" })}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <TimeByProjectChart entries={entries} />
+      </CardContent>
+    </Card>
+  );
+};
+
+export const TimeEntryStatsCardSkeleton = () => {
+  return (
+    <Card size="sm">
+      <CardHeader>
+        <CardTitle>
+          <Skeleton className="h-5.5 w-28" />
+        </CardTitle>
+        <CardDescription className="max-sm:hidden">
+          <Skeleton className="h-5 w-56" />
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <TimeByProjectChartSkeleton />
+      </CardContent>
+    </Card>
+  );
+};
