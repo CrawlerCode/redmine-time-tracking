d38c21deb80cf5c749dd3b2a7633bb672e94bce9
CrawlerCode <41094392+CrawlerCode@users.noreply.github.com>
2026-07-10 18:06:18 +0200

    fix: Fix type errors

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -51,7 +51,7 @@ function Calendar({
           captionLayout === "label" ? "text-sm" : "flex items-center gap-1 rounded-(--cell-radius) text-sm [&>svg]:size-3.5 [&>svg]:text-muted-foreground",
           defaultClassNames.caption_label
         ),
-        table: "w-full border-collapse",
+        month_grid: cn("w-full border-collapse", defaultClassNames.month_grid),
         weekdays: cn("flex", defaultClassNames.weekdays),
         weekday: cn("flex-1 rounded-(--cell-radius) text-[0.8rem] font-normal text-muted-foreground select-none", defaultClassNames.weekday),
         week: cn("mt-2 flex w-full", defaultClassNames.week),
